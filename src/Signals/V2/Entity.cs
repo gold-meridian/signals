f8cfe8791e40d1b4b703212f4bb19e64fe2841f7
mayli4 <sydsleeper@gmail.com>
2026-01-01 00:40:26 -0500

    wip: start signals v2

---
+++ src/Signals/V2/Entity.cs
@@ -0,0 +1,1 @@
+﻿namespace Signals.V2;
---
+++ src/Signals/V2/World.cs
@@ -0,0 +1,20 @@
+﻿using Standard;
+using System.Collections.Concurrent;
+
+namespace Signals.V2;
+
+public class World {
+    internal struct EntityMetadata {
+        public uint Generation;
+        public Bitset256 Mask;
+    }
+    
+    public int NextEntityIndex = 0;
+
+    public BitmaskArray256 EntityPresenceMasks = new();
+    public Bitset256[] EntityComponentMasks = Array.Empty<Bitset256>();
+    public uint[] EntityGenerations = Array.Empty<uint>();
+    public ConcurrentBag<int> FreeEntityIndices = new();
+
+    private uint worldId;
+}
