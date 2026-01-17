2206b07e0029343374c266479d4ebdf9520cee99
mayli4 <sydsleeper@gmail.com>
2026-01-17 17:59:17 -0500

    feat: basic tests

---
+++ src/Tests/ComponentTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Signals.V2;
+
+namespace Tests;
+
+[TestFixture]
+public class ComponentTests {
+    [Test]
+    public void Test_ComponentIdsAreUniquePerType() {
+        int id1 = ComponentStore.GetId<Struct1>();
+        int id2 = ComponentStore.GetId<Struct2>();
+        int id3 = ComponentStore.GetId<int>();
+
+        Assert.That(id1, Is.Not.EqualTo(id2));
+        Assert.That(id1, Is.Not.EqualTo(id3));
+    }
+
+    [Test]
+    public void Test_ComponentGenericIdSame() {
+        Assert.That(ComponentStore.GetId<Struct1>(), Is.EqualTo(ComponentStore.GetId(typeof(Struct1))));
+    }
+}
---
+++ src/Tests/EntityTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Signals.V2;
+
+namespace Tests;
+
+[TestFixture]
+public class EntityTests {
+    [Test]
+    public void Test_ProperEntityCreation() {
+        using var world = new World();
+
+        var e = world.Create();
+        
+        Assert.That(e.IsAlive);
+    }
+    
+    [Test]
+    public void Test_EntityCtorCreatesNull() {
+        Assert.That(new Entity(), Is.EqualTo(default(Entity)));
+    }
+}
